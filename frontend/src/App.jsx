be3e47813f0a4d9eb69f8298d165f04b1458bb0c
BrandonChao12 <al219349@alumnos.uacj.mx>
2025-11-13 12:45:52 -0700

    Preparando rama carritos para merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header/Header';
 import Cuerpo from './components/Cuerpo/Cuerpo';
 import Footer from './components/Footer/Footer';
+import Payment from "./components/Payment";
+import Carrito from "./components/Carrito/Carrito";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Cuerpo />
+
+      <Routes>
+        <Route path="/" element={<Cuerpo />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/carrito" element={<Carrito />} />
+      </Routes>
+
       <Footer />
-    </div>
+    </Router>
   );
 }
 
