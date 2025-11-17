c1313155df5e7988987fee972f551212bdd23111
RaulEstebanAnilesMacias222802 <al222802@alumnos.uacj.mx>
2025-11-17 15:54:10 -0700

    Función para proteger las páginas que requieren inicio de sesión

---
+++ frontend/src/components/routes/AppRoutes.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Inicio } from '../../pages/inicio';
+import { Catalogo } from '../../pages/catalogo';
+import { ProductDetail } from '../../pages/producto_detallado';
+import { Auth } from '../auth';
+import { Carrito } from '../../pages/carrito';
+import { CheckoutPage } from '../../pages/checkout';
+import { OrderConfirmationPage } from '../../pages/confirmation';
+import { FAQ } from '../../pages/estatico/faq';
+import { TermsAndConditions } from '../../pages/estatico/terminos_condiciones';
+import { PaymentMethods } from '../../pages/estatico/metodo_pago';
+import { MiCuenta } from '../../pages/perfil/mi_cuenta';
+import { MisProductos } from '../../pages/perfil/mis_productos';
+import { MisPedidos } from '../../pages/perfil/mis_pedidos';
+import { MiInformacion } from '../../pages/perfil/mi_informacion';
+import { Seguridad } from '../../pages/perfil/mi_seguridad';
+import { Favorites } from '../../pages/perfil/mis_favoritos';
+import Artistas from '../../pages/artistas/Artistas';
+import WriteReview from '../../pages/resenas/crearResena';
+import ProductReviews from '../../pages/resenas/resenasProducto';
+import { ArtistProfile } from '../../pages/artista_perfil';
+import Protected from './Protected';
+
+/**
+ * AppRoutes centraliza las rutas de la aplicación.
+ * Las rutas que requieren autenticación están agrupadas dentro
+ * de un <Route element={<Protected/>}> para protegerlas.
+ */
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Inicio />} />
+      <Route path="/catalogo" element={<Catalogo />} />
+      <Route path="/artistas" element={<Artistas />} />
+      <Route path="/artista/:artistId" element={<ArtistProfile />} />
+      <Route path="/categorias" element={<Inicio />} />
+      <Route path="/producto/:productId" element={<ProductDetail />} />
+      <Route path="/producto/:productId/resenas" element={<ProductReviews />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/orden-confirmada" element={<OrderConfirmationPage />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="/terminos-condiciones" element={<TermsAndConditions />} />
+
+      {/* Rutas protegidas */}
+      <Route element={<Protected />}>
+        <Route path="/metodos-pago" element={<PaymentMethods />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/carrito" element={<Carrito />} />
+        <Route path="/mi-cuenta" element={<MiCuenta />} />
+        <Route path="/mi-cuenta/mis-productos" element={<MisProductos />} />
+        <Route path="/mi-cuenta/mis-pedidos" element={<MisPedidos />} />
+        <Route path="/mi-cuenta/favoritos" element={<Favorites />} />
+        <Route path="/mi-cuenta/informacion" element={<MiInformacion />} />
+        <Route path="/mi-cuenta/seguridad" element={<Seguridad />} />
+        <Route path="/mi-cuenta/pedidos/:orderId/resena/:productId" element={<WriteReview />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
