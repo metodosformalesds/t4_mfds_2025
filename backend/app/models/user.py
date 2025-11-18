755d522d10eb7d773f485c85a126a9b24d6881c6
RaulEstebanAnilesMacias222802 <al222802@alumnos.uacj.mx>
2025-11-17 23:31:45 -0700

    Agregando atrinutos al usuario en modelo

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -12,6 +12,9 @@ class User(Base):
     
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True, nullable=False)
+    full_name = Column(String(100), nullable=False)
+    bio =  Column(Text)
+    address = Column(String(255), nullable=False)
     email = Column(String(100), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
     phone = Column(String(20))
